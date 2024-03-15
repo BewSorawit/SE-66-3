95e3d8f12860258c48d877e1ef0088e6e997947d
aunlaphat <aunlaphat5@hotmail.com>
2024-03-15 19:02:22 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = require('./config/express');
 const db = require('./database/db');
 const { readdirSync } = require('fs');
 
+
 const port = process.env.PORT || 3001;
 
 readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)));
