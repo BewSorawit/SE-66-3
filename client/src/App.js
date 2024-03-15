1ecdc43ec8d438d5d463a12874e150b1d54ea325
aunlaphat <aunlaphat5@hotmail.com>
2024-03-15 17:50:19 +0700

    อัพเดท App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 // client/src/App.js
 import React from 'react';
 import Index from './components/index';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SendToManager from './SendToManager';
+import FCviews from './FCviews';
+import AfterSend from './AfterSend';
 
 function App() {
   return (
     <div className="App">
       <Index />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<FCviews />}></Route>
+          <Route path="/send/:branchID" element={<SendToManager />}></Route>
+          <Route path='/AfterSend' element={<AfterSend />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
