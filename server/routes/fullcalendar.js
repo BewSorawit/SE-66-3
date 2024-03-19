ab45eb61e9b8fc69c8c3cc303043b7c5cf435e37
Brown <104961142+BewSorawit@users.noreply.github.com>
2024-03-19 18:06:33 +0700

    optimize
    ทำให้ไม่มี error ที่ log

--- server/routes/fullcalendar.js
+++
@@ -1,8 +0,0 @@
-// project/server/routes/fullcalendar.js
-const express = require('express');
-const router = express.Router();
-const { createEvent } = require('../controllers/fullcalendar');
-
-router.post('/events', createEvent);
-
-module.exports = router;
