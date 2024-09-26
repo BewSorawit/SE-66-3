49a1e21427e3c2763a8691e64f2354062724dbd8
Brown <sorawitchueachual70@gmail.com>
2024-09-27 01:23:04 +0700

    done jwt backend

--- server/routes/otpRoutes.js
+++ server/routes/otpRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { sendOTP } = require("../controllers/otpController");
 const { confirmUserCreation } = require("../controllers/userController");
-const router = express.Router();
+const authMiddleware = require("../Middlewares/authMiddleware");
 
+const router = express.Router();
 router.post("/otp/send", sendOTP);
-router.get("/otp/confirm", confirmUserCreation);
+router.get("/otp/confirm", authMiddleware, confirmUserCreation);
 
 module.exports = router;
